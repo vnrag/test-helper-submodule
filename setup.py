99734bfc2f9cd5c41af0dea162120fafce8cbdfc
Manjil <shr@vnr.de>
2022-02-21 17:54:57 +0100

    revert

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 from setuptools import setup, find_packages
 
+# Package meta-data.
+NAME = "cdgator"
+DESCRIPTION = "Public VNR package with most commonly used functions"
+URL = "https://github.com/vnrag/test-helper-submodule.git"
+VERSION = "0.1.0"
+REQUIRES_PYTHON = ">=3.8"
+
+
+# What packages are required for this module to be executed?
+REQUIRED = []
+
+
 setup(
-    name="cdgator",
-    version="0.0.1",
-    author="VNR",
-    description="Public VNR package with most commonly used functions",
-    url="https://github.com/vnrag/test-helper-submodule.git",
-    package_dir={"": "cdgator"},
-    packages=find_packages(where="cdgator", exclude=("test",)),
-    python_requires=">=3.8",
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    license="MIT",
+    packages=find_packages(exclude=("test",)),
+    install_requires=REQUIRED,
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
