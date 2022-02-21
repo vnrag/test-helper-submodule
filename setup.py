c2092d89647f3cdb7fe093bde343b499cdcea9b7
Manjil <shr@vnr.de>
2022-02-21 18:12:26 +0100

    iam roles

--- setup.py
+++ setup.py
@@ -7,11 +7,6 @@ URL = "https://github.com/vnrag/test-helper-submodule.git"
 VERSION = "0.1.0"
 REQUIRES_PYTHON = ">=3.8"
 
-
-# What packages are required for this module to be executed?
-REQUIRED = []
-
-
 setup(
     name=NAME,
     version=VERSION,
@@ -20,7 +15,6 @@ setup(
     url=URL,
     license="MIT",
     packages=find_packages(exclude=("test",)),
-    install_requires=REQUIRED,
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3.8",
